1a435effecf4f09bb952c7f69b0c8200db83dbce
pawps <henschelogbonna@gmail.com>
2023-02-19 22:21:42 +0100

    feat: Add entry form for user input

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -8,10 +8,12 @@ app_name = 'learning_logs'
 urlpatterns = [
     # Home page
     path('', views.index, name='index'),
-    # Page that shows all topics.
+    # Page that shows all topics
     path('topics/' ,views.topics, name='topics'),
-    # Detail page for a single topic.
+    # Detail page for a single topic. # All entries in a topic
     path('topics/<int:topic_id>/', views.topic, name='topic'),
     # Page for adding a new topic
     path('new_topic/', views.new_topic, name='new_topic'),
+    # Page for adding a new entry
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
 ]
