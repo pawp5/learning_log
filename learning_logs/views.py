da5ac24ca97200d7d12c6cc0613f7c0df9405dac
pawps <henschelogbonna@gmail.com>
2023-03-17 00:07:58 +0100

    Refactor: Refactor user authentication to check_topic_owner function. Fix: Associate new entry with current user to prevent unauthorized access.

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -9,6 +9,11 @@ def index(request):
     """The home page for Learning Log."""
     return render(request, 'learning_logs/index.html')
 
+def check_topic_owner(request, topic):
+    """Raise exception if  current user is not owner"""
+    if topic.owner != request.user:
+        raise Http404
+    
 @login_required
 def topics(request):
     """Show all topics"""
@@ -20,9 +25,7 @@ def topics(request):
 def topic(request, topic_id):
     """Show a single topic and all its entries."""
     topic = Topic.objects.get(id=topic_id)
-    # Make sure the topic belong to the current user.
-    if topic.owner != request.user:
-        raise Http404
+    check_topic_owner(request, topic)
     
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
@@ -51,6 +54,7 @@ def new_topic(request):
 def new_entry(request, topic_id):
     """Add a new entry for a particular topic."""
     topic = Topic.objects.get(id=topic_id)
+    check_topic_owner(request, topic)
 
     if request.method != 'POST':
         # No data submitted; create a blank form.
@@ -73,8 +77,7 @@ def edit_entry(request, entry_id):
     """Edit an existing entry."""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
-    if topic.owner != request.user:
-        raise Http404
+    check_topic_owner(request, topic)
     
     if request.method != 'POST':
         # Initial request; pre-fill form  with the current entry.
